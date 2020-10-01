52900eb77e869dbeba372fba4f831631730cfea0
Luke Benstead <kazade@gmail.com>
2020-10-01 16:03:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     include_package_data=True,
     # dependencies
     install_requires=[
-        'django-gcloud-connectors>=0.2.3',
+        'django-gcloud-connectors>=0.2.5',
         'google-api-python-client>=1.7.11',
         'google-cloud-tasks>=1.5.0,<2.0.0',
         # requests required by cloud storage file backend
