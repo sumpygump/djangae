035636c868b318642e396e18cb522017cac2e19b
Purvi Trivedi <purvi.trivedi@potatolondon.com>
2020-10-21 10:38:44 +0100

    Fixed white space linter error

--- djangae/contrib/googleauth/backends/__init__.py
+++ djangae/contrib/googleauth/backends/__init__.py
@@ -12,12 +12,12 @@ def _find_atomic_decorator(model):
 
     # FIXME: When Django GCloud Connectors gets rid of its own atomic decorator
     # the Django atomic() decorator can be used regardless
-    
+
     if connection.settings_dict['ENGINE'] == 'gcloudc.db.backends.datastore':
         from gcloudc.db.transaction import atomic
     else:
         from django.db.transaction import atomic
-    
+
     return atomic
 
 
