f530728accafa220710cc1164fd722fc3d0d138c
Paolo Chillari <paolo@potatolondon.com>
2021-05-13 13:58:57 +0100

    Make flake8 happy #1326

--- djangae/sandbox.py
+++ djangae/sandbox.py
@@ -234,6 +234,8 @@ def enable_test_environment_variables():
 def wipe_cloud_storage():
     storage_emulator_host = os.environ['STORAGE_EMULATOR_HOST']
     if not storage_emulator_host:
-        return logger.warning("Cloud storage emulator wipe operation failed. Cloud storage emulator has not been started.")
+        return logger.warning(
+            "Cloud storage emulator wipe operation failed."
+            "Cloud storage emulator has not been started.")
 
     urlopen(f"{storage_emulator_host}/wipe?keep-buckets=true")
