9c394aea7f2a36c89e7c426421bee03f16d5c7c9
Jacob Gur <jacob@fareclock.com>
2022-07-08 15:49:35 -0400

    remove old deferred task view handler no longer in use

--- djangae/views.py
+++ djangae/views.py
@@ -3,9 +3,7 @@ from importlib import import_module
 
 from django.conf import settings
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 
-from djangae import environment
 from djangae.tasks import decorators
 from djangae.core.signals import module_started, module_stopped
 
@@ -35,42 +33,6 @@ def start(request):
 def stop(request):
     module_stopped.send(sender=__name__, request=request)
     return HttpResponse("OK")
-
-
-@csrf_exempt
-def deferred(request):
-    from google.appengine.ext.deferred.deferred import (
-        run,
-        SingularTaskFailure,
-        PermanentTaskFailure
-    )
-
-    response = HttpResponse()
-
-    if 'HTTP_X_APPENGINE_TASKEXECUTIONCOUNT' in request.META:
-        logger.debug("[DEFERRED] Retry %s of deferred task", request.META['HTTP_X_APPENGINE_TASKEXECUTIONCOUNT'])
-
-    if 'HTTP_X_APPENGINE_TASKNAME' not in request.META:
-        logger.critical('Detected an attempted XSRF attack. The header "X-AppEngine-Taskname" was not set.')
-        response.status_code = 403
-        return response
-
-    in_prod = environment.is_production_environment()
-
-    if in_prod and request.META.get("REMOTE_ADDR") != "0.1.0.2":
-        logger.critical('Detected an attempted XSRF attack. This request did not originate from Task Queue.')
-        response.status_code = 403
-        return response
-
-    try:
-        run(request.body)
-    except SingularTaskFailure:
-        logger.debug("Failure executing task, task retry forced")
-        response.status_code = 408
-    except PermanentTaskFailure:
-        logger.exception("Permanent failure attempting to execute task")
-
-    return response
 
 
 @decorators.task_only
